381081997df2ea1f1d0c52f464bfb5d4a981f203
Guillermo Alfaro <galfarotolon@gmail.com>
2020-11-18 21:07:35 -0500

    working on cards using material UI, Alan software already setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards'
+
+
+
+const alanKey = process.env.REACT_APP_API_KEY
 
 function App() {
+
+  const [newsArticles, setNewsArticles] = useState([])
+
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles)
+        }
+      }
+
+    })
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>NEWS APP</h1>
+      <NewsCards articles={newsArticles} />
+
     </div>
   );
 }
---
+++ src/components/NewsCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import useStyles from './styles'
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+
+
+
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, idx }) => {
+
+    const classes = useStyles();
+
+    return (
+        <Card clasName={classes.card}>
+            <CardActionArea href={url} target='_blank'>
+                <CardMedia className={classes.media} image={urlToImage || 'no image available'} />
+                <div clasName={classes.details}>
+                    <Typography variant='body2' color='textSecondary' component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant='body2' color='textSecondary' component='h2'>{source.name}</Typography>
+                </div>
+                <Typography clasName={classes.title} gutterBottom variant='h5'>{title}</Typography>
+                <CardContent>
+
+                    <Typography variant='body2' color='textSecondary' component='p'>{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions clasName={classes.cardActions}>
+                <Button size='small' color='primary'>Learn More</Button>
+                <Typography variant='h5' color='textSecondary'>{idx + 1}</Typography>
+            </CardActions>
+
+        </Card>
+    )
+}
+
+export default NewsCard
